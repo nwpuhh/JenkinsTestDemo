c643d798d4d7885c88c6324a1c556da07491fc53
nwpuhh <huhao_insa@icloud.com>
2018-11-15 11:48:56 +0100

    Add the SimpleClass.java and its .class file

---
+++ SimpleClass.java
@@ -0,0 +1,14 @@
+public class SimpleClass {
+	
+	public int sum(int a, int b) {
+		
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		SimpleClass sc = new SimpleClass();
+
+		System.out.println("The sum of 4+5 is " + sc.sum(4, 5));
+	}
+
+}
